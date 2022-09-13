82ce43fe419ac6a745aeefc3a169f08d90ea4859
eddygulled <edgargulay69@gmail.com>
2022-09-13 14:27:58 +0300

    suggestion view added

--- nisevie/advisor/urls.py
+++ nisevie/advisor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view,advisor_view,advisor_record_incomes, advisor_record_expenses
+from .views import home_view,advisor_view,advisor_record_incomes, advisor_record_expenses, advisor_suggestion_view
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('advisor/', advisor_view, name='advisor'),
     path('streams/', advisor_record_incomes, name='income_streams'),
     path('expenses/', advisor_record_expenses, name='expenses_streams'),
+    path('suggestions/', advisor_suggestion_view, name='suggestions'),
 ]
