605270ab547fbe854c22bdcad2a35d6b3ca4b3c9
eddygulled <edgargulay69@gmail.com>
2022-09-15 18:48:41 +0300

    create plan view listing added

--- nisevie/advisor/views.py
+++ nisevie/advisor/views.py
@@ -20,7 +20,7 @@ def home_view(request):
     bank_account = BankAccount.objects.get(account_holder = customer)
 
     # saving plans 
-    saving_plans = SavingPlan.objects.get(target_account = bank_account)
+    saving_plans = SavingPlan.objects.filter(target_account = bank_account)
     context = {
         'plans': saving_plans
     }
@@ -127,17 +127,13 @@ def advisor_suggestion_view(request):
     return render(request, 'advisor/suggestions.html', context)
 
 # 🐛
-def PlanCreatorPage(request):
-    if request.method == 'POST':
-        time_interval = request.POST['interval']
-        income_stream = request.POST['income']
-        initial_amount = request.POST['amount']
-        deposit_frequency = request.POST['frequency']
-        new_plan = SavingPlan(time_interval=time_interval,
-                              income_stream=income_stream,
-                              initial_amount=initial_amount,
-                              deposit_frequency=deposit_frequency)
-        new_plan.save()
+def plan_create_view(request):
+    holder_id = request.user.id
+    customer = AccountHolder.objects.get(id=holder_id)
+    bank_account = BankAccount.objects.get(account_holder = customer)
 
-    return render(request, )
+    # saving plans 
+    saving_plans = SavingPlan.objects.filter(target_account = bank_account)
+    
+    return render(request, 'advisor/create_plan.html', {'plans' : saving_plans})
 
